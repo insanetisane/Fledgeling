6d0c8fec8a904f3fcaa5124cc3c1eef752f07352
Dillon Headley <dillon@pilotinteractive.ca>
2017-11-13 11:39:10 -0500

    utils.pug and other tweaks

--- src/static/service-worker.js
+++ src/static/service-worker.js
@@ -1,8 +1,8 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('main')  
+    caches.open('main')
     .then(cache => cache.addAll([
-      '/assets/css/reboot.css',
+      '/assets/css/base.css',
       '/assets/css/global.css',
     ]))
   )
@@ -12,7 +12,12 @@ self.addEventListener('fetch', event => {
   const request = event.request
 
   event.respondWith(
-    caches.match(request)
-      .then(response => response || fetch(request))
+    caches.open('main').then((cache) => cache.match(event.request)
+      .then((response) => response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      );
+    )
   )
 })
